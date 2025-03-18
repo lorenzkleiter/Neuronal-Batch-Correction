df64741e784770b5f6ab2cc58d9823d1c4d1f8aa
lorenzkleiter <lorenz.kleiter@tum.de>
2025-03-18 18:23:39 +0100

    updated data structure

---
+++ src/models/__init__.py.py
@@ -0,0 +1,4 @@
+# This is a valid, empty __init__.py file
+# No code needed here
+
+#__init__.py files make each directory a Python package, allowing you to import modules using dot notation:
---
+++ src/training/final_training.py
@@ -0,0 +1,6 @@
+#---Final Training---
+# Autoencoder, Cell Classifiers and  discriminator are trained together as all last step
+# updating all model’s weights, apart from the discriminator
+
+#imports
+from utils import loading
