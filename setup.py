830bf236d4ce7786d3689255f71156bc951f0294
lorenzkleiter <lorenz.kleiter@tum.de>
2025-04-14 22:37:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="neuronal-batch-correction",
-    version="0.1.0",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
-        "tensorflow>=2.19.0",
-        "scanpy>=1.11.1",
-        "scib>=1.1.7",
+        "tensorflow==2.19.0",
+        "scanpy==1.11.1",
+        "scib==1.1.7",
         "numpy",
         "scipy",
         "anndata",
         "matplotlib",
         "pandas",
     ],
-    author="Your Name",
-    author_email="your.email@example.com",
     description="An autoencoder-based neural network for batch correction in single-cell data",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -23,10 +21,9 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
-    python_requires=">=3.10",
+    python_requires="==3.10",
 )
